1d0681e705f057602928c9ea18b9705207269899
Asaf Strassberg <asafst@microsoft.com>
2018-05-29 12:51:38 +0300

    Added some configuration for travis ci, fixed some linting issues

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # vsts-pull-request-creator
-A simple command line tool that creates a VSTS PR on your current git repository
+
+> A simple command line tool that creates a VSTS PR on your current git repository
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-import program from 'commander';
-import {PullReuqestCreator} from './PullRequestCreator';
 import clipboardy from "clipboardy";
+import program from "commander";
 import {InputParams} from "./DTOs";
-
-program
-    .option('-t, --title <title>', 'The title of the Pull Request')
-    .option('-d, --description <prdescription>', 'The description of the Pull Request')
-    .parse(process.argv);
+import {PullReuqestCreator} from "./PullRequestCreator";
 
 async function runAsync(title: string): Promise<string> {
+
+    program
+        .option("-t, --title <title>", "The title of the Pull Request")
+        .option("-d, --description <prdescription>", "The description of the Pull Request")
+        .parse(process.argv);
 
     const inputParams = new InputParams();
     inputParams.title = program.title;
@@ -17,20 +17,13 @@ async function runAsync(title: string): Promise<string> {
 
     const prCreator = new PullReuqestCreator();
     const prLink = await prCreator.createPullRequest(inputParams);
-    await clipboardy.write(prLink)
+    await clipboardy.write(prLink);
 
     return prLink;
 
 }
 runAsync(program.title).then((prLink) => {
-    console.log(`PR link was copied to your clipboard: ${prLink}`)
-}).catch(reason => {
-    console.log(`Creation of PR failed due to: ${reason}`)
-})
-
-
-
-
-
-
-
+    console.log(`PR link was copied to your clipboard: ${prLink}`);
+}).catch((reason) => {
+    console.log(`Creation of PR failed due to: ${reason}`);
+});
